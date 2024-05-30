3f902c68678b1cfa1c719f5a5a2dedb7d1063ca7
Pallavi S <115915060+mlpgsjvd@users.noreply.github.com>
2024-05-30 15:53:30 +0530

    Add files via upload

---
+++ ArithassignEx.java
@@ -0,0 +1,23 @@
+package operator;
+
+public class ArithassignEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10,b=3;
+		a+=b;//a=a+b;
+		System.out.println(a);
+		a-=b;
+		System.out.println(a);
+		a*=b;
+		System.out.println(a);
+		a=a/b;
+		System.out.println(a);
+		a=a%b;
+		System.out.println(a);
+		
+
+	}
+
+}
