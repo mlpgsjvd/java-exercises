4f54a2662ba8522db03625eac507706075b93120
Pallavi S <115915060+mlpgsjvd@users.noreply.github.com>
2024-06-03 17:51:50 +0530

    Add files via upload

---
+++ Palindrom.java
@@ -0,0 +1,43 @@
+package operator;
+
+import java.util.Scanner;
+
+public class Palindrom {
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        
+	        System.out.print("Enter a string: ");
+	        String input = scanner.nextLine();
+
+	        boolean isPalindrome = checkPalindrome(input);
+
+	        
+	        if (isPalindrome) {
+	            System.out.println("The string \"" + input + "\" is a palindrome.");
+	        } else {
+	            System.out.println("The string \"" + input + "\" is not a palindrome.");
+	        }
+
+	        scanner.close();
+	    }
+
+	   
+	    public static boolean checkPalindrome(String str) {
+	        int left = 0;
+	        int right = str.length() - 1;
+
+	        while (left < right) {
+	            
+	            if (str.charAt(left) != str.charAt(right)) {
+	                return false;
+	            }
+	            left++;
+	            right--;
+	        }
+
+	        return true;
+	    }
+	}
+
