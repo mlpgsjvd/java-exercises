47c3411a874f509904c60951d697cf8975904654
Pallavi S <115915060+mlpgsjvd@users.noreply.github.com>
2024-06-03 15:30:15 +0530

    Add files via upload

---
+++ SimpleIf.java
@@ -0,0 +1,25 @@
+package operator;
+
+import java.util.Scanner;
+
+public class SimpleIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				Scanner sc=new Scanner(System.in);
+
+		      String isGraduated;
+		      System.out.println("Are you a Graduate ? Yes/No");
+		      isGraduated=sc.next();
+		      if(isGraduated.equals("Yes"))
+		      {
+		    	  System.out.println("You are Eligible");
+		      }
+		     
+
+			}
+
+
+
+	}
