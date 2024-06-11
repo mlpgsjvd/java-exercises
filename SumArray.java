3fe733987250f2093498973d602244b9a23f80f1
Pallavi S <115915060+mlpgsjvd@users.noreply.github.com>
2024-06-11 17:09:25 +0530

    Add files via upload

---
+++ SumArray.java
@@ -0,0 +1,19 @@
+package array;
+import java.util.*;
+public class SumArray {
+	
+	
+	    public static void main(String[] args) {
+	        int[] numbers = {1, 2, 3, 4, 5};       
+	       int sum = 0;
+	        for (int i = 0; i < numbers.length; i++) {
+	            sum += numbers[i];
+	        }
+	        System.out.println("The sum of the array elements is: " + sum);
+	    }
+}
+
+	
+
+
+
